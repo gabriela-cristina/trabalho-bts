63cd2456862f5fb28769f63cf1d4a689755a5288
Gabriela <80904845+gabriela-cristina@users.noreply.github.com>
2021-07-05 15:12:00 -0300

    Add files via upload

---
+++ html/quiz/jav.js
@@ -0,0 +1,283 @@
+function correta1(){
+    let botao = document.getElementById('escolha1');
+    document.getElementById('escolha1').style.background = 'rgb(11, 223, 36)';
+    let escolher = document.getElementById('escolher');
+    var i = setInterval(function () {
+
+        if (escolher = escolha1){
+        window.location.assign("./pergunta2.html")
+        }
+    }, 800);
+}
+
+function falso11(){
+    let botao = document.getElementById('falso11');
+    document.getElementById('falso11').style.background = 'rgb(224, 11, 11, 1)';
+    var i = setInterval(function () {
+    if (escolhas = escolha1){
+        window.location.assign("./pergunta2.html")
+        }
+    }, 800);
+}
+
+function falso12(){
+    let botao = document.getElementById('falso12');
+    document.getElementById('falso12').style.background = 'rgb(224, 11, 11, 1)';
+    var i = setInterval(function () {
+    if (escolhas = escolha1){
+        window.location.assign("./pergunta2.html")
+        }
+    }, 800);
+}
+
+function falso13(){
+    let botao = document.getElementById('falso13');
+    document.getElementById('falso13').style.background = 'rgb(224, 11, 11, 1)';
+    var i = setInterval(function () {
+    if (escolhas = escolha1){
+        window.location.assign("./pergunta2.html")
+        }
+    }, 800);
+}
+
+function falso14(){
+    let botao = document.getElementById('falso14');
+    document.getElementById('falso14').style.background = 'rgb(224, 11, 11, 1)';
+    var i = setInterval(function () {
+    if (escolhas = escolha1){
+        window.location.assign("./pergunta2.html")
+        }
+    }, 800);
+}
+
+function correta2(){
+    let botao = document.getElementById('escolha1');
+    document.getElementById('escolha1').style.background = 'rgb(11, 223, 36)';
+    let escolher = document.getElementById('escolher');
+    var i = setInterval(function () {
+
+        if (escolher = escolha1){
+        window.location.assign("./pergunta3.html")
+        }
+    }, 800);
+}
+
+function falso21(){
+    let botao = document.getElementById('falso21');
+    document.getElementById('falso21').style.background = 'rgb(224, 11, 11, 1)';
+    var i = setInterval(function () {
+
+    if (escolhas = escolha1){
+        window.location.assign("./pergunta3.html")
+        }
+    }, 800);
+}
+
+function falso22(){
+    let botao = document.getElementById('falso22');
+    document.getElementById('falso22').style.background = 'rgb(224, 11, 11, 1)';
+    var i = setInterval(function () {
+
+    if (escolhas = escolha1){
+        window.location.assign("./pergunta3.html")
+        }
+    }, 800);
+}
+
+function falso23(){
+    let botao = document.getElementById('falso23');
+    document.getElementById('falso23').style.background = 'rgb(224, 11, 11, 1)';
+    var i = setInterval(function () {
+
+    if (escolhas = escolha1){
+        window.location.assign("./pergunta3.html")
+        }
+    }, 800);
+}
+
+function falso24(){
+    let botao = document.getElementById('falso24');
+    document.getElementById('falso24').style.background = 'rgb(224, 11, 11, 1)';
+    var i = setInterval(function () {
+
+    if (escolhas = escolha1){
+        window.location.assign("./pergunta3.html")
+        }
+    }, 800);
+}
+
+function correta3(){
+    let botao = document.getElementById('escolha1');
+    document.getElementById('escolha1').style.background = 'rgb(11, 223, 36)';
+    let escolher = document.getElementById('escolher');
+    var i = setInterval(function () {
+
+        if (escolher = escolha1){
+        window.location.assign("./pergunta4.html")
+        }
+    }, 800);
+}
+
+function falso31(){
+    let botao = document.getElementById('falso31');
+    document.getElementById('falso31').style.background = 'rgb(224, 11, 11, 1)';
+    var i = setInterval(function () {
+
+    if (escolhas = escolha1){
+        window.location.assign("./pergunta4.html")
+        }
+    }, 800);
+}
+
+function falso32(){
+    let botao = document.getElementById('falso32');
+    document.getElementById('falso32').style.background = 'rgb(224, 11, 11, 1)';
+    var i = setInterval(function () {
+
+    if (escolhas = escolha1){
+        window.location.assign("./pergunta4.html")
+        }
+    }, 800);
+}
+
+function falso33(){
+    let botao = document.getElementById('falso33');
+    document.getElementById('falso33').style.background = 'rgb(224, 11, 11, 1)';
+    var i = setInterval(function () {
+
+    if (escolhas = escolha1){
+        window.location.assign("./pergunta4.html")
+        }
+    }, 800);
+}
+
+function falso34(){
+    let botao = document.getElementById('falso34');
+    document.getElementById('falso34').style.background = 'rgb(224, 11, 11, 1)';
+    var i = setInterval(function () {
+
+    if (escolhas = escolha1){
+        window.location.assign("./pergunta4.html")
+        }
+    }, 800);
+}
+
+function correta4(){
+    let botao = document.getElementById('escolha1');
+    document.getElementById('escolha1').style.background = 'rgb(11, 223, 36)';
+    let escolher = document.getElementById('escolher');
+    var i = setInterval(function () {
+
+        if (escolher = escolha1){
+        window.location.assign("./pergunta5.html")
+        }
+    }, 800);
+}
+
+function falso41(){
+    let botao = document.getElementById('falso41');
+    document.getElementById('falso41').style.background = 'rgb(224, 11, 11, 1)';
+    var i = setInterval(function () {
+
+    if (escolhas = escolha1){
+        window.location.assign("./pergunta5.html")
+        }
+    }, 800);
+}
+
+function falso42(){
+    let botao = document.getElementById('falso42');
+    document.getElementById('falso42').style.background = 'rgb(224, 11, 11, 1)';
+    var i = setInterval(function () {
+
+    if (escolhas = escolha1){
+        window.location.assign("./pergunta5.html")
+        }
+    }, 800);
+}
+
+function falso43(){
+    let botao = document.getElementById('falso43');
+    document.getElementById('falso43').style.background = 'rgb(224, 11, 11, 1)';
+    var i = setInterval(function () {
+
+    if (escolhas = escolha1){
+        window.location.assign("./pergunta5.html")
+        }
+    }, 800);
+}
+
+function falso44(){
+    let botao = document.getElementById('falso44');
+    document.getElementById('falso44').style.background = 'rgb(224, 11, 11, 1)';
+    var i = setInterval(function () {
+
+    if (escolhas = escolha1){
+        window.location.assign("./pergunta5.html")
+        }
+    }, 800);
+}
+
+function correta5(){
+    let botao = document.getElementById('escolha1');
+    document.getElementById('escolha1').style.background = 'rgb(11, 223, 36)';
+    let escolher = document.getElementById('escolher');
+    var i = setInterval(function () {
+
+        if (escolher = escolha1){
+        window.location.assign("./final.html")
+        }
+    }, 800);
+}
+
+function falso51(){
+    let botao = document.getElementById('falso51');
+    document.getElementById('falso51').style.background = 'rgb(224, 11, 11, 1)';
+    var i = setInterval(function () {
+
+    if (escolhas = escolha1){
+        window.location.assign("./final.html")
+        }
+    }, 800);
+}
+
+function falso52(){
+    let botao = document.getElementById('falso52');
+    document.getElementById('falso52').style.background = 'rgb(224, 11, 11, 1)';
+    var i = setInterval(function () {
+
+    if (escolhas = escolha1){
+        window.location.assign("./final.html")
+        }
+    }, 800);
+}
+
+function falso53(){
+    let botao = document.getElementById('falso53');
+    document.getElementById('falso53').style.background = 'rgb(224, 11, 11, 1)';
+    var i = setInterval(function () {
+
+    if (escolhas = escolha1){
+        window.location.assign("./final.html")
+        }
+    }, 800);
+}
+
+function falso54(){
+    let botao = document.getElementById('falso54');
+    document.getElementById('falso54').style.background = 'rgb(224, 11, 11, 1)';
+    var i = setInterval(function () {
+
+    if (escolhas = escolha1){
+        window.location.assign("./final.html")
+        }
+    }, 800);
+}
+
+
+
+
+
+
+
+
